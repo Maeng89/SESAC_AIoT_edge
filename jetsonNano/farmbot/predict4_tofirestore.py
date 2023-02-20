241cb2788143747a28cdbee074097c198bd92277
maeng <ggmaeng@gmail.com>
2023-02-20 10:37:59 +0800

    0.2v jetson to firestore

--- jetsonNano/farmbot/predict4_tofirestore.py
+++ jetsonNano/farmbot/predict4_tofirestore.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 from time import sleep
 from datetime import datetime
-import collections
+from collections import Counter
 
 import os
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 import tensorflow as tf
-model_predict = tf.saved_model.load('./model/converea')
+growth_model= tf.saved_model.load('./model/converea')
 
 def gstreamer_pipeline(
     sensor_id=0,
@@ -38,41 +38,33 @@ def gstreamer_pipeline(
     )
 
 def show():
-    pred_list = []
     num = 0
     window_title = 'converea'
     video_capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     if video_capture.isOpened():
         try:
             window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
-                        
+            
             while True:
                 num += 1
                 ret_val, frame = video_capture.read()
-                
                 if frame is None:
                     print('empty frame, reboot please', num)
                     sleep(1)
                 else:
                     if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
                         cv2.imshow(window_title, frame)
+                        
                         try : 
                             #img = cv2.imread(removed)
                             img = frame[0:460, 80:560]
                             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                             img = cv2.resize(img, (150,150)) / 255.0 # inputµ¥ÀÌÅÍ¸¦ ½ºÄÉÀÏ¸µÇß±â ¶§¹®¿¡ ¿¹ÃøÇÒ ¶§¿¡µµ ½ºÄÉÀÏ¸µ ÇØ¾ßÇÔ
                             
-                            pred = model_predict([img])
-                            print(pred)
-                            growth_level = str(np.argmax(pred, axis=1))
-                            pred_list.append(growth_level)
-                            
-                            #pred = np.asarray(pred)
-                            #growth_level = str(np.argmax(pred, axis=1)[0])
-                        
+                            pred = growth_model([img])
+                            #print(pred)
+                            growth_level = str(np.argmax(pred, axis=1)[0])
                             print(num, ' growth level', growth_level)
-                            
-                            
 
                         except Exception as e:
                             print(e)
@@ -80,7 +72,15 @@ def show():
                     else:
                         print('????')
                         
-
+                # firestore upload
+                upt = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+                gl = Counter(growth_level).most_common(1)[0][0]
+                result = {'growth_level' : gl,
+                          'update_time' : upt}
+                #model_result.append([gl, upt])
+                print(result)
+                upload_growth(result) #  [('cherry', 4)] 
+                
                 keyCode = cv2.waitKey(10) & 0xFF
                 if keyCode == 27 or keyCode == ord('q'):
                     video_capture.release()
@@ -88,9 +88,9 @@ def show():
                     doc_ref.update({'is_running': False})
                     sleep(1)
                     break
+                
+                sleep(interval)
             # while loop finish
-            upload_growth(counts.most_common(1)[0][0]) #  [('cherry', 4)] kust
-            
             
         finally:
             doc_ref.update({'is_running': False})
@@ -103,6 +103,8 @@ def show():
         
 
 
+
+
 if __name__ == "__main__":
     
     ######## fire base ########
@@ -110,7 +112,7 @@ if __name__ == "__main__":
     from firebase_admin import credentials, firestore
     # option
     db_collection = 'converea' # database collection id
-    db_id = '0.1v' # database document id
+    db_id = '0.2v' # database document id
     db_key = 'nugunaaiot-maeng-1004a11a5af7.json'
     # Cloud Database : firestore
     cred = credentials.Certificate(f'../../secret/{db_key}')
@@ -118,7 +120,7 @@ if __name__ == "__main__":
     db = firestore.client()
 
     def create_device(db_id):
-        # doc_ref = db.collection(db_collection).document(db_id)
+        doc_ref = db.collection(db_collection).document(db_id)
         doc = doc_ref.get()
         if doc.exists :
             pass
@@ -127,22 +129,21 @@ if __name__ == "__main__":
                 'id': db_id,
                  'is_running': True,
                  'manufacture_date': datetime.now(),
-                 'growth_level':[]
+                 'model':[],
+                 'sensor':[]
             })        
 
-    def upload_growth(growth_level):
+    def upload_growth(result):
         #doc_ref = db.collection(db_collection).document(db_id)
-        doc_ref.update({'is_running': True, 'growth_level': firestore.ArrayUnion([growth_level])})
+        doc_ref.update({'is_running': True, 'model': firestore.ArrayUnion([result])})
         print('data upload')
     
     create_device(db_id)
     doc_ref = db.collection(db_collection).document(db_id)
     
-
     # option
     interval = 2 # sec
     
     while True :    
         show()
-        sleep(interval)
         
